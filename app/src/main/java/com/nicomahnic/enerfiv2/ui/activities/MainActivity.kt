449ce37a13f1ffc418818f2616e933eb224c9d81
Nicolas Mahnic <nico.mahnic94@gmail.com>
2022-01-12 23:33:57 -0300

    Themes

--- app/src/main/java/com/nicomahnic/enerfiv2/ui/activities/MainActivity.kt
+++ app/src/main/java/com/nicomahnic/enerfiv2/ui/activities/MainActivity.kt
@@ -3,13 +3,17 @@ package com.nicomahnic.enerfiv2.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nicomahnic.enerfiv2.R
+import com.nicomahnic.enerfiv2.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
