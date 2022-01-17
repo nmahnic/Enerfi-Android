ec8cf60274518aedd30affc929f1baf34ada8b2b
Nicolas Mahnic <nico.mahnic94@gmail.com>
2022-01-16 22:17:06 -0300

    improve navigation

--- app/src/main/java/com/nicomahnic/enerfiv2/ui/activities/MainActivity.kt
+++ app/src/main/java/com/nicomahnic/enerfiv2/ui/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nicomahnic.enerfiv2.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,16 +22,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        supportActionBar?.hide()
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.logoutFragment, R.id.homeFragment, R.id.deviceRegisterFragment)
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.navView.setupWithNavController(navController)
     }
 }
